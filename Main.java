0abb71fd721d29c0131cb223ea94460f68a6a55f
Perl Magdalena Maria <s58734@edu.campus02.at>
2026-04-13 11:58:51 +0200

    gv

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Spiel uno = new Spiel();
 
+        System.out.println("TEST");
+        System.out.println("TEST");
+
     }
 }
