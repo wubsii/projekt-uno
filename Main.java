39b8e041ebcf410a8bbc68b4d5391e8d404960e1
wubsii <magdi.perl@gmail.com>
2026-05-19 15:55:57 +0200

    initGame (Main aufräumen)

--- Main.java
+++ Main.java
@@ -4,46 +4,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
-        DiscardPile discardPile = new DiscardPile();
-        Spiel uno = new Spiel(discardPile);
-
-        uno.shuffle();
-
-        Spieler[] spielerListe = new Spieler[4];
-
-        List<String> vorhandeneNamen = new ArrayList<>();
-
-        for (int i = 0; i < 4; i++) {
-
-            spielerListe[i] = new Spieler(discardPile);
-
-            String name = spielerListe[i].gettingName(vorhandeneNamen);
-
-            spielerListe[i].setName(name);
-
-            vorhandeneNamen.add(name);
-        }
-
-        // Karten einmal verteilen
-        for (int i = 0; i < 4; i++) {
-            spielerListe[i].setHand(uno.dealInitialHand(7));
-        }
-
-        // Startspieler
-        int start = spielerListe[0].randomizePlayer();
-
-        while (true) {
-
-            Spieler aktuellerSpieler = spielerListe[(start++) % 4];
-
-            Card topCard = uno.getStartercard();
-
-            Menu.setSpieler(aktuellerSpieler);
-            Menu.setSpiel(uno);
-
-            // Menu.showMenu();
-            Menu.runMenu();
-        }
+        Spiel.initGame();
     }
 }
