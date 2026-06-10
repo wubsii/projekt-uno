cf2a579132b9fde63ea85887f87ce433b6b7d096
magdi <magdi.perl@gmail.com>
2026-06-10 12:46:31 +0200

    Hinzufügen von Kommentaren und "Verschönern" des Codes

--- Card.java
+++ Card.java
@@ -1,30 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Card {
     public Value value;
     public Color color;
+    private char chosenColor;
 
+    // Konstruktor
     public Card(Value v, Color c) {
         value = v;
         color = c;
     }
 
+    // Damit wir die Karten mit der methode PRINT() ausgeben können
+    public void print() {
+        System.out.println(color + " " + value);
+    }
+
+    // Gibt einfach den Wert der Enumeration zurück
     public int getPointValue() {
         return value.getPointValue();
     }
 
-    public static List<Card> makeDeck() {
-        List<Card> deck = new ArrayList<>();
-        for (Color c : Color.values()) {
-            for (Value v : Value.values()) {
-                deck.add(new Card(v, c));
-            }
-        }
-        return deck;
-    }
-
-    // Override toString to return colored output in the format | value |
+    // Überschreibt die toString()-Methode und gibt die Karte farbig im Format "| wert |" zurück.
     @Override
     public String toString() {
         String ansiColor = getAnsiColor(color);
@@ -32,7 +27,7 @@ public class Card {
         return ansiColor + "| " + value.toString().toLowerCase() + " |" + reset;
     }
 
-    // Helper method to get ANSI color code for a given Color
+    // Hilfsmethode zur Ermittlung des passenden ANSI-Farbcodes für eine Kartenfarbe.
     private String getAnsiColor(Color color) {
         switch (color) {
             case RED:    return "\u001B[31m"; // Red
@@ -44,30 +39,23 @@ public class Card {
         }
     }
 
-    // Getters and setters
-    private char chosenColor;
-
+    // Setzt die aktuell ausgewählte Farbe
     public void setChosenColor(char color) {
         this.chosenColor = color;
     }
 
+    // Gibt die aktuell ausgewählte Farbe zurück
     public char getChosenColor() {
         return chosenColor;
     }
 
+    // Gibt den Wert dieser Karte zurück
     public Value getValue() {
         return value;
     }
 
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
+    // Gibt die Farbe dieser Karte zurück
     public Color getColor() {
         return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
     }
 }
--- Deck.java
+++ Deck.java
@@ -2,26 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
+
     public static List<Card> makeDeck() {
-        List<Card> deck = new ArrayList<Card>();
-        for (Color c : Color.values()) { // alle farben loopen
-            // schwarze erstellen
+        // Neue Liste für das Kartendeck erstellen
+        List<Card> deck = new ArrayList<>();
+
+        // Alle verfügbaren Farben durchlaufen
+        for (Color c : Color.values()) {
+
+            // Schwarze Jokerkarten erzeugen
             if (c == Color.BLACK) {
+
+                // Je 4 Farbwahl- und +4-Karten hinzufügen
                 for (int i = 0; i < 4; i++) {
                     deck.add(new Card(Value.COLOR_CHANGE, c));
                     deck.add(new Card(Value.PLUS_FOUR, c));
                 }
+
             } else {
-                // alle die 1 mal vorkommen erstellen
+
+                // Die 0-Karte gibt es pro Farbe nur einmal
                 deck.add(new Card(Value.ZERO, c));
             }
 
-            for (int i = 0; i < 2 ; i++) {
+            // Alle anderen Karten kommen pro Farbe zweimal vor
+            for (int i = 0; i < 2; i++) {
+
+                // Für schwarze Karten keine weiteren Karten erzeugen
                 if (c != Color.BLACK) {
+
+                    // Aktionskarten hinzufügen
                     deck.add(new Card(Value.SKIP, c));
                     deck.add(new Card(Value.REVERSE, c));
                     deck.add(new Card(Value.PLUS_TWO, c));
 
+                    // Zahlenkarten 1 bis 9 hinzufügen
                     deck.add(new Card(Value.ONE, c));
                     deck.add(new Card(Value.TWO, c));
                     deck.add(new Card(Value.THREE, c));
@@ -35,6 +50,7 @@ public class Deck {
             }
         }
 
+        // Vollständiges Kartendeck zurückgeben
         return deck;
     }
 }
